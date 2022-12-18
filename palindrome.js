f66b384865bc0c9bfeecb513d481678f94421862
Hafiz Jawad Amjad <amjadjaw@corp.contoursoftware.net>
2022-12-18 16:57:31 +0500

    Task 3 10 Characters Condition

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,14 @@
 function isPalindrome(string){
 
-    reverseString = "";
+
+    if(string.length > 10 ){
+        document.getElementById('result').innerHTML = "String is More Than 10 Characters";
+    }
+    else if(string.length == 1 || string.length == 0){
+        document.getElementById('result').innerHTML = "Please Enter at least 2 charcters";
+    }
+    else{
+        reverseString = "";
     for(let i=string.length-1;i>=0;i--){
         reverseString = reverseString + string[i];
     }
@@ -12,5 +20,12 @@ function isPalindrome(string){
     else{
         document.getElementById('result').innerHTML = "String is not Palindrome";
     }
+    }
 
-}
+
+
+        
+    }
+
+    
+
